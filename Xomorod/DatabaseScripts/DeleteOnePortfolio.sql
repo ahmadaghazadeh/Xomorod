6df2eec12c1e9c0fb5a57e527d3f61150726db27
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-11-18 23:47:35 +0330

    add delete portfolio sql script

---
+++ Xomorod/DatabaseScripts/DeleteOnePortfolio.sql
@@ -0,0 +1,17 @@
+﻿DECLARE @Id UNIQUEIDENTIFIER = N'0A78F7F6-A45E-45CC-BDFD-9FB958CEBF72' -- Portfolio Id 
+
+DELETE FROM Xomorod.dbo.PortfolioCategories
+WHERE PortfolioId = @id
+
+UPDATE Xomorod.dbo.Portfolios
+SET	IconId = NULL
+WHERE Id = @Id
+
+DELETE FROM Xomorod.dbo.Resources
+WHERE ElementUniqueId = @id
+
+DELETE FROM Xomorod.dbo.ExtraLinks
+WHERE PortfolioId = @Id
+
+DELETE FROM Xomorod.dbo.Portfolios
+WHERE Id = @id
