0913ce752ba96cfb1367485bae62c7429709c98b
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-12-25 18:18:49 +0330

    change navbar color to #ddd

--- Xomorod/js/xomorod.js
+++ Xomorod/js/xomorod.js
@@ -18,7 +18,7 @@
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
-    })
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function () {
