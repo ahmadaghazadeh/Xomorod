fec99aad236c0aacc93e6e63c4130f284db3a1fc
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2016-02-20 22:45:11 +0330

    add api documentation

--- src/Xomorod.API/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ src/Xomorod.API/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -33,17 +33,17 @@ namespace Xomorod.API.Areas.HelpPage
             Justification = "Part of a URI.")]
         public static void Register(HttpConfiguration config)
         {
-            //// Uncomment the following to use the documentation from XML documentation file.
-            //config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
+            // Uncomment the following to use the documentation from XML documentation file.
+            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
 
-            //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
-            //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
-            //// formats by the available formatters.
-            //config.SetSampleObjects(new Dictionary<Type, object>
-            //{
-            //    {typeof(string), "sample string"},
-            //    {typeof(IEnumerable<string>), new string[]{"sample 1", "sample 2"}}
-            //});
+            // Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
+            // Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
+            // formats by the available formatters.
+            config.SetSampleObjects(new Dictionary<Type, object>
+            {
+                {typeof(string), "sample string"},
+                {typeof(IEnumerable<string>), new string[]{"sample 1", "sample 2"}}
+            });
 
             // Extend the following to provide factories for types not handled automatically (those lacking parameterless
             // constructors) or for which you prefer to use non-default property values. Line below provides a fallback
@@ -52,16 +52,16 @@ namespace Xomorod.API.Areas.HelpPage
             config.GetHelpPageSampleGenerator().SampleObjectFactories.Add(GeneratePageResult);
 #endif
 
-            //// Extend the following to use a preset object directly as the sample for all actions that support a media
-            //// type, regardless of the body parameter or return type. The lines below avoid display of binary content.
-            //// The BsonMediaTypeFormatter (if available) is not used to serialize the TextSample object.
-            //config.SetSampleForMediaType(
-            //    new TextSample("Binary JSON content. See http://bsonspec.org for details."),
-            //    new MediaTypeHeaderValue("application/bson"));
+            // Extend the following to use a preset object directly as the sample for all actions that support a media
+            // type, regardless of the body parameter or return type. The lines below avoid display of binary content.
+            // The BsonMediaTypeFormatter (if available) is not used to serialize the TextSample object.
+            config.SetSampleForMediaType(
+                new TextSample("Binary JSON content. See http://bsonspec.org for details."),
+                new MediaTypeHeaderValue("application/bson"));
 
-            //// Uncomment the following to use "[0]=foo&[1]=bar" directly as the sample for all actions that support form URL encoded format
-            //// and have IEnumerable<string> as the body parameter or return type.
-            //config.SetSampleForType("[0]=foo&[1]=bar", new MediaTypeHeaderValue("application/x-www-form-urlencoded"), typeof(IEnumerable<string>));
+            // Uncomment the following to use "[0]=foo&[1]=bar" directly as the sample for all actions that support form URL encoded format
+            // and have IEnumerable<string> as the body parameter or return type.
+            config.SetSampleForType("[0]=foo&[1]=bar", new MediaTypeHeaderValue("application/x-www-form-urlencoded"), typeof(IEnumerable<string>));
 
             //// Uncomment the following to use "1234" directly as the request sample for media type "text/plain" on the controller named "Values"
             //// and action named "Put".
