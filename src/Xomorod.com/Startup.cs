5c89912313e1483050932da1cc185e9ee97534ce
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2016-02-21 23:16:42 +0330

    New project xomorod.com added

---
+++ src/Xomorod.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Xomorod.com.Startup))]
+namespace Xomorod.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
