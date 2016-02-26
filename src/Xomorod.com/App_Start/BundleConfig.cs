54ae91f32f230db35ae9644ea459f9d41660e7b6
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2016-02-26 22:40:13 +0330

    Fix page top state problem

--- src/Xomorod.com/App_Start/BundleConfig.cs
+++ src/Xomorod.com/App_Start/BundleConfig.cs
@@ -8,7 +8,53 @@ namespace Xomorod.com
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/mainLayoutTop").Include(
+            RegisterScriptBundles(bundles);
+            RegisterStyleBundles(bundles);
+            
+            BundleTable.EnableOptimizations = true;
+        }
+
+        private static void RegisterStyleBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/font").Include("~/Content/front.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Homemade_Apple",
+                @"//fonts.googleapis.com/css?family=Lato%3A300%2C400%2C700%2C400italic%7CMontserrat%3A700%7CHomemade+Apple").Include(
+                "~/Content/Homemade_Apple.css"));
+
+            bundles.Add(new StyleBundle("~/Content/OpenSans",
+                @"//fonts.googleapis.com/css?family=Open+Sans%3A400%2C300%2C300italic%2C400italic%2C600italic%2C600%2C700%2C700italic%2C800%2C800italic").Include(
+                "~/Content/OpenSans.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/woocommerce").Include(
+                      "~/Content/woocommerce-layout.css",
+                      "~/Content/woocommerce-smallscreen.css",
+                      "~/Content/woocommerce.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap", @"http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css").Include(
+                      "~/Content/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/font_awesome").Include("~/Content/font-awesome.min.cs"));
+
+            bundles.Add(new StyleBundle("~/Content/style").Include("~/Content/style.cs"));
+
+            bundles.Add(new StyleBundle("~/Content/blog").Include("~/Content/select2.css"));
+
+            bundles.Add(new StyleBundle("~/Content/mainLayoutT").Include(
+                      "~/Content/front.css",
+                      "~/Content/woocommerce-layout.css",
+                      "~/Content/woocommerce-smallscreen.css",
+                      "~/Content/woocommerce.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/style.css"));
+        }
+
+        private static void RegisterScriptBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/mainLayoutT").Include(
                         "~/Scripts/jquery.js",
                         "~/Scripts/jquery-migrate.min.js",
                         "~/Scripts/scripts.js",
@@ -17,19 +63,7 @@ namespace Xomorod.com
             bundles.Add(new ScriptBundle("~/bundles/blog").Include(
                         "~/Scripts/select2.js"));
 
-            bundles.Add(new StyleBundle("~/Content/blog").Include(
-                        "~/Content/select2.css"));
-
-            bundles.Add(new StyleBundle("~/Content/mainLayoutTop").Include(
-                      "~/Content/front.css",
-                      "~/Content/woocommerce-layout.css",
-                      "~/Content/woocommerce-smallscreen.css",
-                      "~/Content/woocommerce.css",
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/font-awesome.min.css",
-                      "~/Content/style.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/mainLayoutBottom").Include(
+            bundles.Add(new ScriptBundle("~/bundles/mainLayoutB").Include(
                         "~/Scripts/jquery.blockUI.min.js",
                         "~/Scripts/woocommerce.min.js",
                         "~/Scripts/jquery.cookie.min.js",
@@ -40,26 +74,22 @@ namespace Xomorod.com
                         "~/Scripts/zerif.js",
                         "~/Scripts/wp-embed.min.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
 
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
-
-
+            bundles.Add(new ScriptBundle("~/bundles/recaptcha", "https://www.google.com/recaptcha/api.js").Include("~/Scripts/api.js"));
         }
     }
 }
