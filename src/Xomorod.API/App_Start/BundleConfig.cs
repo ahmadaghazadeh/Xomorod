bd9c1bf87eb6f4b850912bc2963ef27c689792bc
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2016-06-16 16:03:50 +0430

    Update BundleConfig.cs

--- src/Xomorod.API/App_Start/BundleConfig.cs
+++ src/Xomorod.API/App_Start/BundleConfig.cs
@@ -7,11 +7,7 @@ namespace Xomorod.API
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-#if DEBUG
-            BundleTable.EnableOptimizations = false;
-#else
-            BundleTable.EnableOptimizations = true;
-#endif
+            BundleTable.EnableOptimizations = !Debugger.IsAttached;
             
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
